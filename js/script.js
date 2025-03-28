538726e6fda20de7b2a5a2163da2f43c8c0bff30
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-03-28 16:38:19 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Catherine All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Catherine Vu
+// Created on: March 25 2025
 // This file contains the JS functions for index.html
 
 /**
- * Displays an alert with the message "Hello, World!"
+ * Calculates hourly waqe and salary
  */
-function myButtonClicked() {
-  alert("Hello, World!")
-}
+function calculateSalary () {
+ let hours = parseFloat(document.getElementById('hours').value);
+            let rate = parseFloat(document.getElementById('rate').value);
+            if (isNaN(hours) || isNaN(rate)) {
+                document.getElementById('output').innerHTML = "Please enter valid numbers";
+                return;
+            }
+            let grossPay = hours * rate;
+            let tax = grossPay * 0.18; // 18% tax
+            let netPay = grossPay - tax;
+            document.getElementById('output').innerHTML = `Your pay will be: $${netPay.toFixed(2)}<br> The government will take: $${tax.toFixed(2)}`;
+        }
