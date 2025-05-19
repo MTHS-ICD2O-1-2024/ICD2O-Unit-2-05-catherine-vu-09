7e62f30971966af1c43357a767cf95a17cae1c0b
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-05-19 02:05:43 +0000

    js

--- js/script.js
+++ js/script.js
@@ -5,18 +5,22 @@
 // This file contains the JS functions for index.html
 
 "use strict"
+
 /**
  * Calculates hourly waqe and salary
  */
 function calculateSalary () {
-  //input 
+
+  //input
   const hoursworked = parseFloat(document.getElementById('hours-worked').value)
   const hourlywage = parseFloat(document.getElementById('hourly-wage').value)
-
+  
   //process
-  const weeklypay = hoursworked * hourlywage * (1 - 0.18)
-  const incometax = hoursworked * hourlywage * 0.18
-  //output 
+  const weeklypay = (hoursworked * hourlywage) * (1.00 - 0.18)
+  const incometax = (hoursworked * hourlywage) * 0.18
+
+  //output
   document.getElementById('weeklypay').innerHTML = 'Your pay will be: $'+ weeklypay.toFixed(2)
   document.getElementById('incometax').innerHTML = 'The government will take: $ ' + incometax.toFixed(2)
-}
+  }
+  
